eadde0a672e281c51b47a403e9e30b50d0dac4ca
Danny Gonzalez <gdanny120@gmail.com>
2018-01-19 15:26:26 -0800

    Finished functionality of web app:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  tasks = [{name: 'wash the dishes', done: 1}, {name: 'laundry', done: null}, {name: 'clean room', done: null}];
+  deletedtasks = [];
+  checkTask(index) {
+    if (this.tasks[index].done === 1) {
+      this.tasks[index].done = null;
+    } else {
+      this.tasks[index].done = 1;
+    }
+  }
+  deleteTask(index) {
+    let task = this.tasks[index];
+    this.tasks.splice(index, 1);
+    this.deletedtasks.push(task);
+  }
+  changeName(name, index) {
+    this.tasks[index].name = name;
+  }
+  createNewTask() {
+    this.tasks.push({name: 'new task', done: null});
+  }
+  customTrackBy(index: number, obj: any): any {
+    return index;
+  }
 }
